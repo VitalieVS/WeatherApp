f84b2fbfa5bd3df1485bd6be3c83832d28832a5e
VitalieVSV <dut66AHJCT>
2021-01-13 19:56:12 +0200

    humidity/weather

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,8 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import org.json.JSONObject;
 
 public class Controller {
 
@@ -21,12 +23,40 @@ public class Controller {
     private ImageView switchTheme;
 
     @FXML
-    void initialize() {
+    private Text cityName;
 
+    @FXML
+    private Text weatherCondition;
+
+    @FXML
+    private Text weatherDescription;
+
+    @FXML
+    private Text temperature;
+
+    @FXML
+    private Text temperatureText;
+
+    @FXML
+    private Text humidity;
+
+    @FXML
+    private Text humidityText;
+
+    @FXML
+    void initialize() {
+        if (!getData().isEmpty()) {
+            JSONObject obj = new JSONObject(getData());
+            cityName.setText(obj.getString("name"));
+            weatherCondition.setText(obj.getJSONArray("weather").getJSONObject(0).getString("main"));
+            weatherDescription.setText(obj.getJSONArray("weather").getJSONObject(0).getString("description"));
+            temperatureText.setText(String.valueOf(obj.getJSONObject("main").getDouble("temp")));
+            humidityText.setText(String.valueOf(obj.getJSONObject("main").getDouble("humidity")));
+        }
     }
 
-    StringBuffer getData() {
-        StringBuffer content = new StringBuffer();
+    private static String getData() {
+        StringBuilder content = new StringBuilder();
         try {
             URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Drochia&appid=5dfbc1c91fc61457629e0d4e30513742");
             URLConnection urlConn = url.openConnection();
@@ -41,11 +71,11 @@ public class Controller {
             System.out.println("Not found");
             e.printStackTrace();
         }
-        return content;
+        return content.toString();
     }
 
     @FXML
     void switchTheme() {
-        System.out.println(getData());
+
     }
 }
