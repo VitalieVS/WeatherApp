00219732dba2f2bc3bd9604f1755b21f3601d96f
VitalieVSV <dut66AHJCT>
2021-01-13 19:25:07 +0200

    api call

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
@@ -24,25 +25,27 @@ public class Controller {
 
     }
 
-    @FXML
-    void switchTheme() {
+    StringBuffer getData() {
         StringBuffer content = new StringBuffer();
         try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=%D0%94%D1%80%D0%BE%D0%BA%D0%B8%D1%8F&appid=5dfbc1c91fc61457629e0d4e30513742");
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Drochia&appid=5dfbc1c91fc61457629e0d4e30513742");
             URLConnection urlConn = url.openConnection();
-
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
             String line;
 
             while ((line = bufferedReader.readLine()) != null) {
-                content.append(line + "\n");
+                content.append(line).append("\n");
             }
             bufferedReader.close();
         } catch (IOException e) {
             System.out.println("Not found");
             e.printStackTrace();
         }
+        return content;
+    }
 
-        System.out.println("clicked!");
+    @FXML
+    void switchTheme() {
+        System.out.println(getData());
     }
 }
